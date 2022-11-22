647c6de4686ef660bd5a05a018f53550f4b2c8e8
hilyas786 <97009108+hilyas786@users.noreply.github.com>
2022-11-23 02:04:37 +0800

    nprogress bar added

--- pages/_app.js
+++ pages/_app.js
@@ -4,15 +4,20 @@ import { ThemeProvider } from 'next-themes';
 import Footer from '../components/Footer';
 import CTA from '../components/CTA';
 import { useRouter } from 'next/router';
-import { ScrollToTop } from '../components/ScrollBackToTop';
+import NextNProgress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   const showCTA = router.pathname === '/contact' ? false : true;
   return (
     <>
+      <NextNProgress
+        color='#3949ab'
+        height={6}
+        showOnShallow={true}
+        options={{ showSpinner: true }}
+      />
       <Navbar />
-
       <Component {...pageProps} />
       {showCTA && <CTA />}
       <Footer />
